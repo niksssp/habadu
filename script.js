f98fdd50129ff4cd07e5f6d93fec2683444c0d4c
niksssp <paulenneneeca@gmail.com>
2025-03-12 03:00:24 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const flame=document.getElementById("flame");
+
+navigator.mediaDevices.getUserMedia({audio:true;}).then((stream)=>{
+  const audioContext=new AudioContext();
+  const analyser=audioContext.createAnalyser();
+  const microphone=audioContext.createMediaStreamSource(stream);microphone.connect(analyser);
+  
+  analyser.fftSize=256;
+  const bufferLength=analysser.frequencyBinCount;
+  const dataArrat=new uint8Array(bufferLength);
+  
+  function detectBlow(){
+    analyser.getByteFrequencyData(dataArray);
+    let maxVolume=Math.max(...dataArray);
+    If (maxVolume>100){
+      flame.style.display="none";
+      alert("yayyyyy");
+    }
+    requestAnimationFrame(detectBlow);
+  }
+  detectBlow();
+});
